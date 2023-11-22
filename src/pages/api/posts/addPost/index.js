3db33079d91e2696fa1eaf7894602bc32b971eeb
leonce-bizindavyi <leonceb900@gmail.com>
2023-11-22 19:27:42 +0200

    first commit

--- src/pages/api/posts/addPost/index.js
+++ src/pages/api/posts/addPost/index.js
@@ -20,7 +20,7 @@ export default async function uploadHandler(req, res) {
     form.uploadDir = path.join(process.cwd(), './public/Thumbnails');
 
     // Parsez la requête
-    form.parse(req, (err, fields, files) => {
+    form.parse(req, async (err, fields, files) => {
       if (err) {
         console.error(err);
         return res.status(500).json({ error: 'Une erreur s\'est produite lors de l\'upload des vidéos.' });
@@ -28,27 +28,28 @@ export default async function uploadHandler(req, res) {
 
       // Récupérez les informations des fichiers uploadés
       const images = Array.isArray(files.image) ? files.image : [files.image];
-      // Déplacez chaque fichier uploadé dans le répertoire de destination
-      if(images[0] == undefined) {
-        insertVideo(fields.oldimage,fields)
-        res.status(200).json({ message: 'Upload réussi !' });
-      }else{
-        const movePromises = images.map((image) => {
-          if(image.newFilename) return moveVideo(image,fields);
-          else return insertVideo(fields.oldimage,fields)
-        });
-        Promise.all(movePromises)
-        .then(() => {
-          res.status(200).json({ message: true });
-        })
-        .catch((error) => {
-          console.error(error);
-          res.status(500).json({ error: 'Une erreur s\'est produite lors de l\'enregistrement des vidéos.' });
-        });
-      }
-      
 
-      
+      // Déplacez chaque fichier uploadé dans le répertoire de destination
+      if (images[0] === undefined) {
+        await insertVideo(fields.oldimage, fields);
+        res.status(200).json({ message: 'Upload réussi !' });
+      } else {
+        const movePromises = images.map(async (image) => {
+          if (image.newFilename) {
+            return moveVideo(image, fields);
+          } else {
+            return insertVideo(fields.oldimage, fields);
+          }
+        });
+
+        try {
+          await Promise.all(movePromises);
+          res.status(200).json({ message: true });
+        } catch (error) {
+          console.error(error);
+          res.status(500).json({ error: 'Une erreur s\'est produite lors de l\'enregistrement des vidéos.', details: error.message });
+        }
+      }
     });
   } else {
     res.setHeader('Allow', 'POST');
@@ -56,22 +57,22 @@ export default async function uploadHandler(req, res) {
   }
 }
 
-async function moveVideo(image,fields) {
-  //insertion dans la base de données
-  insertVideo(fields,image.newFilename)
-  //modifier dans le dossier
-  const newPath = `./public/Thumbnails/${image.newFilename}`
-  await insertVideo(image.newFilename,fields);
+async function moveVideo(image, fields) {
+  // Modifier dans le dossier
+  const newPath = `./public/Thumbnails/${image.newFilename}`;
+  await insertVideo(image.newFilename, fields);
   return newPath;
 }
 
-async function insertVideo(image,fields) {
-  const {title,desc,cat,id} = fields
+async function insertVideo(image, fields) {
+  const { title, desc, cat, id } = fields;
   try {
     // Exécutez la requête SQL pour insérer une vidéo dans la base de données
     const rows = await executeQuery('UPDATE posts SET Title=?, Image=?, Body=?, Categorie=?, Visible=? WHERE ID=?',
-    [title, image, desc, cat, 1, id]);
-
+      [title, image, desc, cat, 1, id]);
   } catch (error) {
+    console.error(error);
+    // Gérer l'erreur, par exemple, renvoyer une réponse d'erreur au client
+    throw error; // Propager l'erreur pour que Promise.all le capture
   }
 }
