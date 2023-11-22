aabb9dcf1241b2768e4aa9c780851ca29fe9207a
leonce-bizindavyi <leonceb900@gmail.com>
2023-11-22 22:21:07 +0200

    first commit

--- src/pages/api/posts/addPost/index.js
+++ src/pages/api/posts/addPost/index.js
@@ -2,6 +2,7 @@ import fs from 'fs-extra';
 import formidable from 'formidable';
 import path from 'path';
 import executeQuery from '@/Config/db4';
+import chokidar from 'chokidar';
 
 export const config = {
   api: {
@@ -9,15 +10,26 @@ export const config = {
   },
 };
 
+const thumbnailsFolderPath = path.join(process.cwd(), './public/Thumbnails');
+
+// Créez un watcher pour surveiller les changements dans le dossier Thumbnails
+const watcher = chokidar.watch(thumbnailsFolderPath);
+
+watcher.on('all', async (event, filePath) => {
+  console.log(`File ${filePath} has been ${event}`);
+  // Réagissez aux changements ici, par exemple, mettre à jour la base de données
+  // ou déclencher une autre action en fonction des changements dans le dossier Thumbnails
+});
+
 export default async function uploadHandler(req, res) {
   if (req.method === 'POST') {
     const form = new formidable.IncomingForm();
 
     // Créez un répertoire "Thumbnails" s'il n'existe pas déjà
-    await fs.ensureDir('./public/Thumbnails');
+    await fs.ensureDir(thumbnailsFolderPath);
 
     // Définissez le répertoire de destination des vidéos uploadées
-    form.uploadDir = path.join(process.cwd(), './public/Thumbnails');
+    form.uploadDir = thumbnailsFolderPath;
 
     // Parsez la requête
     form.parse(req, async (err, fields, files) => {
@@ -28,27 +40,30 @@ export default async function uploadHandler(req, res) {
 
       // Récupérez les informations des fichiers uploadés
       const images = Array.isArray(files.image) ? files.image : [files.image];
+
       // Déplacez chaque fichier uploadé dans le répertoire de destination
-      if(images[0] == undefined) {
-        await insertVideo(fields.oldimage,fields)
+      if (images[0] === undefined) {
+        await insertVideo(fields.oldimage, fields);
         res.status(200).json({ message: 'Upload réussi !' });
-      }else{
-        const movePromises = images.map(async(image) => {
-          if(image.newFilename) return await moveVideo(image,fields);
-          else return await insertVideo(fields.oldimage,fields)
+      } else {
+        const movePromises = images.map(async (image) => {
+          if (image.newFilename) {
+            await moveVideo(image, fields);
+          } else {
+            await insertVideo(fields.oldimage, fields);
+          }
         });
-        Promise.all(movePromises)
-        .then(() => {
+
+        try {
+          await Promise.all(movePromises);
+          
+          // Répondez au client une fois que tous les fichiers ont été traités
           res.status(200).json({ message: true });
-        })
-        .catch((error) => {
+        } catch (error) {
           console.error(error);
           res.status(500).json({ error: 'Une erreur s\'est produite lors de l\'enregistrement des vidéos.' });
-        });
+        }
       }
-      
-
-      
     });
   } else {
     res.setHeader('Allow', 'POST');
@@ -56,20 +71,22 @@ export default async function uploadHandler(req, res) {
   }
 }
 
-async function moveVideo(image,fields) {
-  //modifier dans le dossier
-  const newPath = `./public/Thumbnails/${image.newFilename}`
-  await insertVideo(image.newFilename,fields);
+async function moveVideo(image, fields) {
+  // Modifier dans le dossier
+  const newPath = path.join(thumbnailsFolderPath, image.newFilename);
+  await insertVideo(image.newFilename, fields);
   return newPath;
 }
 
 async function insertVideo(image, fields) {
+  const { title, desc, cat, id } = fields;
   try {
-    const { title, desc, cat, id } = fields;
-    // Exécutez la requête SQL pour mettre à jour une vidéo dans la base de données
-    const rows = await executeQuery('CALL editPost(?,?,?,?,?)' ,[title,image,desc,cat,id]);
-    // Autres opérations après la mise à jour de la vidéo
+    // Exécutez la requête SQL pour insérer une vidéo dans la base de données
+    const rows = await executeQuery('CALL editPost(?,?,?,?,?)', [title, image, desc, cat, id]);
+    // Autres opérations après l'insertion de la vidéo
   } catch (error) {
-    // Gérez les erreurs ici
+    console.error(error);
+    // Gérer l'erreur ici
+    throw error; // Propager l'erreur pour que Promise.all le capture
   }
 }
